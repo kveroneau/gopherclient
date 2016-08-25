4a8f49ee11cf99101f54cee24e7837e8659c50c9
kveroneau <unknown>
2016-08-24 20:27:28 -0600

    Initial commit of Delphi GopherClient.

---
+++ gopherclient.pas
@@ -0,0 +1,137 @@
+unit gopherclient;
+
+{$mode objfpc}{$H+}{$R+}{$Q+}
+
+interface
+
+uses
+  Classes, ssockets, SysUtils, strutils;
+
+Type
+
+  GopherType = (Text, Menu, CSO, Error, MacBinHex, PCBinHex, UUEncoded, Index,
+                Telnet, Binary, Duplicate, Sound, Event, Calendar, HTML, TN3270,
+                MIME, IMAGE, WHOIS, QUERY, GIF, WWW);
+
+  TGopherMenu = record
+    gtype: char;
+    name: String;
+    selector: String;
+    host: String;
+    port: Integer;
+  end;
+
+  AGopherMenu = Array of TGopherMenu;
+  PGopherMenu = ^TGopherMenu;
+
+  { TGopherClient }
+
+  TGopherClient = class(TComponent)
+  Private
+    FHost: String;
+    FPort: Integer;
+    Procedure GetEntry(data: TMemoryStream; entry: TStringList);
+  Public
+    Constructor Create(AOwner: TComponent); override;
+    Destructor Destroy; override;
+    Procedure SetHost(Const host: String; Const port: Integer);
+    Function SendSelector(Const selector: String; Const query: String): TMemoryStream;
+    Function SendSelector(Const selector: String; Const text: Boolean): TMemoryStream;
+    Function GetMenu(Const selector: String): AGopherMenu;
+  end;
+
+implementation
+
+{ TGopherClient }
+
+procedure TGopherClient.GetEntry(data: TMemoryStream; entry: TStringList);
+var
+  line: String;
+  b: Byte;
+begin
+  line:='';
+  Repeat
+    b:=data.ReadByte;
+    if (b<>10) and (b<>13) then
+      line:=line+chr(b);
+  until (b=10);
+  entry.Clear;
+  entry.Delimiter:=#9;
+  entry.StrictDelimiter:=True;
+  entry.DelimitedText:=line;
+end;
+
+constructor TGopherClient.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+end;
+
+destructor TGopherClient.Destroy;
+begin
+  inherited Destroy;
+end;
+
+procedure TGopherClient.SetHost(const host: String; const port: Integer);
+begin
+  FHost:=host;
+  FPort:=port;
+end;
+
+function TGopherClient.SendSelector(const selector: String; const query: String
+  ): TMemoryStream;
+begin
+  Result:=SendSelector(selector+#9+query, False);
+end;
+
+function TGopherClient.SendSelector(const selector: String; Const text: Boolean): TMemoryStream;
+var
+  sock: TInetSocket;
+  buf: String;
+  size: Integer;
+begin
+  Result:=TMemoryStream.Create;
+  if text then
+    Result.Write('<pre>', 5);
+  buf := selector+#13#10;
+  sock:=TInetSocket.Create(FHost, FPort);
+  sock.Write(buf[1], Length(buf));
+  buf:='';
+  SetLength(buf, 4096);
+  Repeat
+    size:=sock.Read(buf[1], 4096);
+    if (size>0) then
+      Result.Write(buf[1], size);
+  until (size=0);
+  sock.Free;
+  if text then
+    Result.Write('</pre>', 6);
+  Result.Seek(0, soBeginning);
+end;
+
+function TGopherClient.GetMenu(const selector: String): AGopherMenu;
+var
+  data: TMemoryStream;
+  entry: TStringList;
+  i: Integer;
+begin
+  SetLength(Result, 0);
+  data:=SendSelector(selector, False);
+  entry:=TStringList.Create;
+  Repeat
+    GetEntry(data, entry);
+    if Length(entry.Strings[0]) > 1 then
+    begin
+      i:=Length(Result);
+      SetLength(Result, i+1);
+      Result[i].gtype:=entry.Strings[0][1];
+      Result[i].name:=RightStr(entry.Strings[0], Length(entry.Strings[0])-1);
+      Result[i].selector:=entry.Strings[1];
+      Result[i].host:=entry.Strings[2];
+      Result[i].port:=StrToInt(entry.Strings[3]);
+    end;
+  until (data.Position>=data.Size);
+  entry.Free;
+end;
+
+end.
+
---
+++ main.pas
@@ -0,0 +1,221 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, IpHtml, Forms, Controls, Graphics, Dialogs,
+  ComCtrls, ExtCtrls, StdCtrls, gopherclient, URIParser;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    EngageBtn: TButton;
+    GopherAddr: TEdit;
+    ImageViewer: TImage;
+    ImageList1: TImageList;
+    IpHtmlPanel1: TIpHtmlPanel;
+    Label1: TLabel;
+    TreeView1: TTreeView;
+    procedure EngageBtnClick(Sender: TObject);
+    procedure FormResize(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure TreeView1Click(Sender: TObject);
+    procedure TreeView1Expanding(Sender: TObject; Node: TTreeNode;
+      var AllowExpansion: Boolean);
+  private
+    GopherItems: Array of PGopherMenu;
+    function GetPointer(MenuItem: TGopherMenu): PGopherMenu;
+    procedure PopulateNode(Node: TTreeNode; host: String; port: Integer; selector: String);
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormShow(Sender: TObject);
+var
+  root: TTreeNode;
+begin
+  root:=TreeView1.Items.Add(nil, 'gopher.veroneau.net:70');
+  root.ImageIndex:=0;
+  root.SelectedIndex:=0;
+  PopulateNode(root, 'gopher.veroneau.net', 70, '');
+end;
+
+procedure TForm1.FormResize(Sender: TObject);
+begin
+  TreeView1.Height:=Height-22;
+  IpHtmlPanel1.Width:=Width-TreeView1.Width;
+  IpHtmlPanel1.Height:=Height-22;
+  ImageViewer.Width:=IpHtmlPanel1.Width;
+  ImageViewer.Height:=IpHtmlPanel1.Height;
+  GopherAddr.Width:=Width-Label1.Width-EngageBtn.Width-5;
+end;
+
+procedure TForm1.EngageBtnClick(Sender: TObject);
+var
+  uri: TURI;
+  gopher: TGopherClient;
+  root: TTreeNode;
+  selector: String;
+begin
+  uri:=ParseURI(GopherAddr.Text, 'gopher', 70, True);
+  if uri.Protocol <> 'gopher' then
+  begin
+    ShowMessage('This client can only view gopherspace!');
+    Exit;
+  end;
+  ImageViewer.Visible:=False;
+  selector:=RightStr(uri.Path, Length(uri.Path)-2)+uri.Document;
+  Case uri.Path[2] of
+    '0':
+      begin
+        gopher:=TGopherClient.Create(Self);
+        gopher.SetHost(uri.Host, uri.Port);
+        IpHtmlPanel1.Visible:=True;
+        IpHtmlPanel1.SetHtmlFromStream(gopher.SendSelector(selector, True));
+        gopher.Free;
+      end;
+    '1':
+      begin
+        root:=TreeView1.Items.Add(nil, uri.Host);
+        root.ImageIndex:=0;
+        root.SelectedIndex:=0;
+        PopulateNode(root, uri.Host, uri.Port, selector);
+      end;
+  end;
+end;
+
+procedure TForm1.TreeView1Click(Sender: TObject);
+var
+  item: PGopherMenu;
+  gopher: TGopherClient;
+  MenuItems: AGopherMenu;
+  data: TMemoryStream;
+  entry: TGopherMenu;
+  buf: String;
+begin
+  if (TreeView1.Selected <> nil) and (TreeView1.Selected.Data <> nil) then
+  begin
+    ImageViewer.Visible:=False;
+    item:=TreeView1.Selected.Data;
+    Caption:='Gopher - '+item^.name;
+    GopherAddr.Text:='gopher://'+item^.host+':'+IntToStr(item^.port)+'/'+item^.gtype+item^.selector;
+    Case item^.gtype of
+      '0':
+        begin
+          gopher:=TGopherClient.Create(Self);
+          gopher.SetHost(item^.host, item^.port);
+          IpHtmlPanel1.Visible:=True;
+          IpHtmlPanel1.SetHtmlFromStream(gopher.SendSelector(item^.selector, True));
+          gopher.Free;
+        end;
+      '1':
+        begin
+          gopher:=TGopherClient.Create(Self);
+          gopher.SetHost(item^.host, item^.port);
+          MenuItems:=gopher.GetMenu(item^.selector);
+          gopher.Free;
+          data:=TMemoryStream.Create;
+          data.Write('<pre>', 5);
+          for entry in MenuItems do
+          begin
+            buf := entry.name+#13#10;
+            data.Write(buf[1], Length(buf));
+          end;
+          data.Write('</pre>', 6);
+          data.Seek(0, soBeginning);
+          IpHtmlPanel1.Visible:=True;
+          IpHtmlPanel1.SetHtmlFromStream(data);
+          data.Free;
+        end;
+      'I':
+        begin
+          gopher:=TGopherClient.Create(Self);
+          gopher.SetHost(item^.host, item^.port);
+          IpHtmlPanel1.Visible:=False;
+          ImageViewer.Visible:=True;
+          ImageViewer.Picture.LoadFromStream(gopher.SendSelector(item^.selector, False));
+          gopher.Free;
+        end;
+    end;
+  end;
+end;
+
+procedure TForm1.TreeView1Expanding(Sender: TObject; Node: TTreeNode;
+  var AllowExpansion: Boolean);
+var
+  item: PGopherMenu;
+begin
+  AllowExpansion:=True;
+  if Node.Data <> nil then
+  begin
+    item:=Node.Data;
+    PopulateNode(Node, item^.host, item^.port, item^.selector);
+  end;
+end;
+
+function TForm1.GetPointer(MenuItem: TGopherMenu): PGopherMenu;
+var
+  i: Integer;
+begin
+  New(Result);
+  with MenuItem do
+  begin
+    Result^.gtype:=gtype;
+    Result^.host:=host;
+    Result^.name:=name;
+    Result^.port:=port;
+    Result^.selector:=selector;
+  end;
+  i:=Length(GopherItems);
+  SetLength(GopherItems, i+1);
+  GopherItems[i] := Result;
+end;
+
+procedure TForm1.PopulateNode(Node: TTreeNode; host: String; port: Integer;
+  selector: String);
+var
+  gopher: TGopherClient;
+  item: TTreeNode;
+  MenuItems: AGopherMenu;
+  MenuItem: TGopherMenu;
+begin
+  SetLength(GopherItems, 0);
+  gopher:=TGopherClient.Create(Self);
+  gopher.SetHost(host, port);
+  MenuItems:=gopher.GetMenu(selector);
+  gopher.Free;
+  for MenuItem in MenuItems do
+    if MenuItem.gtype <> 'i' then
+    begin
+      item := TreeView1.Items.AddChild(Node, MenuItem.name);
+      if MenuItem.gtype <> '1' then
+      begin
+        item.ImageIndex:=1;
+        item.SelectedIndex:=1;
+        item.Data:=GetPointer(MenuItem);
+      end
+      else
+      begin
+        item.ImageIndex:=0;
+        item.SelectedIndex:=0;
+        item.Data:=GetPointer(MenuItem);
+        item.HasChildren:=True;
+      end;
+    end;
+end;
+
+end.
+
